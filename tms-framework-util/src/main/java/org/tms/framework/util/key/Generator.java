d6f097ac11db49e394ccc826a3480fd59a80728f
jwt-git <git@juweitang.net>
2014-08-22 10:29:09 +0800

    添加util代码

---
+++ tms-framework-util/src/main/java/org/tms/framework/util/key/Generator.java
@@ -0,0 +1,22 @@
+/**
+ * 生成器接口
+ * 
+ */
+
+package org.tms.framework.util.key;
+
+
+/**
+ * @author sam.zhang
+ *
+ */
+public interface Generator {
+
+	/**
+	 * 根据输入参数生成对应的结果
+	 * @param strings
+	 * @return
+	 */
+	public String generate(String ...strings);
+	
+}
